72f0663e7cbd856fb402c401d62bae383f51cefb
adityaaj54 <aditya.aj54@gmail.com>
2021-11-02 10:26:21 -0400

    Added new static methods and optimized for lint

--- calculator/main.py
+++ calculator/main.py
@@ -1,21 +1,25 @@
 """ This is the increment function"""
 class Calculator:
     """ This is the Calculator class"""
+    @staticmethod
+    def add_number(value_a,value_b):
+        """ adds numbers"""
+        return value_a + value_b
 
-    result = 0
-    def get_result(self):
-        """ Get Result of Calculation"""
-        return self.result
+    @staticmethod
+    def subtract_number( value_a ,value_b):
+        """ subtract number two numbers"""
+        return value_a - value_b
 
-    def add_number(self, value_a):
-        """ adds number to result"""
-        self.result = self.result + value_a
-        return self.result
-    def subtract_number(self, value_a):
-        """ subtract number from result"""
-        self.result = self.result - value_a
-        return self.result
-    def multiply_numbers(self, value_a, value_b):
-        """ multiply two numbers and store the result"""
-        self.result = value_a * value_b
-        return self.result
+    @staticmethod
+    def multiply_numbers(value_a, value_b):
+        """ multiply two numbers and return the result"""
+        return value_a * value_b
+
+    @staticmethod
+    def divide_numbers(value_a , value_b):
+        """ divide two numbers"""
+        try:
+            return value_a / value_b
+        except ZeroDivisionError as err:
+            raise ZeroDivisionError from err
