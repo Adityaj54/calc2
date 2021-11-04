800bbac98134d696e79212c63aefc86af7a645f4
adityaaj54 <aditya.aj54@gmail.com>
2021-11-04 19:53:06 -0400

    Added new static methods and optimized for lint

--- calc/division.py
+++ calc/division.py
@@ -8,11 +8,14 @@ inherits the value A and value B from the calculation class"""
 from calc.calculation import Calculation
 
 #This is how you extend the Addition class with the Calculation
-class Multiplication(Calculation):
+class Division(Calculation):
     """The addition class has one method to
     get the result of the the calculation A and B come from
     the calculation parent class"""
     def get_result(self):
         """you need to use self to reference the
          data contained in the instance of the object.  This is encapsulation"""
-        return self.value_a * self.value_b
+        try:
+            return self.value_a / self.value_b
+        except ZeroDivisionError as err:
+            raise ZeroDivisionError from err
